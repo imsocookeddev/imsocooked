c880f33e171c36d26a600433f13a1dd604865436
Christian Walker <christiandwalker2021@gmail.com>
2024-10-10 17:03:04 -0500

    fix db import

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -3,9 +3,13 @@ import {drizzle} from 'drizzle-orm/neon-http'
 import { config } from "dotenv";
 import * as schema from './schema'
 
+
+
 config({path: "../../.env"});
 
 export * from "drizzle-orm";
+export * from "./functions";
+
 const sql = neon(process.env.DATABASE_URL!)
 
 export const db = drizzle(sql,{schema})

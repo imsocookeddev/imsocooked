5b653a5e27ad63e166f81b08e5751aba9d28ec4b
Christian Walker <christiandwalker2021@gmail.com>
2024-10-10 00:01:58 -0500

    updates config

--- packages/config/cooked.config.ts
+++ packages/config/cooked.config.ts
@@ -1,9 +1,13 @@
-const c = {
+export default {
   name:"I'm So Cooked",
-  roles:{
-
-  }
-}
-
-
-export default c;
+  roles:[
+    "user",
+    "admin"
+  ],
+  problem_types:[
+    "mulitple_choice",
+    "drag-n-drop",
+    "matching",
+    "single_word_response",
+  ],
+} as const;

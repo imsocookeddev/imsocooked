6dd60c4f3f35834342d202bcefe5b18695ff46f7
Christian Walker <christiandwalker2021@gmail.com>
2024-10-20 17:07:54 -0500

    adds routes for rest of links

--- apps/admin/app/cuisines/page.tsx
+++ apps/admin/app/cuisines/page.tsx
@@ -1,7 +1,7 @@
-export default function UsersPage(){
+export default function CuisinesPage() {
   return (
     <main className="w-screen flex flex-row h-[calc(100dvh-4rem)] items-center justify-center">
-      <h1 className="font-black text-5xl">Users</h1>
+      <h1 className="font-black text-5xl">Cuisines</h1>
     </main>
   );
 }
--- apps/admin/app/problems/page.tsx
+++ apps/admin/app/problems/page.tsx
@@ -1,7 +1,7 @@
-export default function UsersPage(){
+export default function ProblemsPage() {
   return (
     <main className="w-screen flex flex-row h-[calc(100dvh-4rem)] items-center justify-center">
-      <h1 className="font-black text-5xl">Users</h1>
+      <h1 className="font-black text-5xl">Problems</h1>
     </main>
   );
 }

940b06f7cd6a4f2b065794b4d4ab443b772bb582
Christian Walker <christiandwalker2021@gmail.com>
2024-10-20 16:58:30 -0500

    updates db package

--- packages/db/functions.ts
+++ packages/db/functions.ts
@@ -1,8 +1,17 @@
-import { db,eq } from ".";
+import { db,eq,and } from ".";
 import { users } from "./schema";
 
 export async function getUser(id:string){
   return db.query.users.findFirst({
-    where:eq(users.clerkID,id)
+    where:eq(users.userID,id)
   })
+}
+
+export async function getAdminUser(id:string){
+  return db.query.users.findFirst({
+    where:(
+      and(eq(users.userID,id), eq(users.role,'admin'))
+    )
+  })
+    
 }

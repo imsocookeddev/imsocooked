61d1117d528095c27651cce3ba2d7e88c9f8d3e7
Christian Walker <christiandwalker2021@gmail.com>
2024-10-20 16:59:24 -0500

    adds config

--- packages/config/cooked.config.ts
+++ packages/config/cooked.config.ts
@@ -1,5 +1,15 @@
 export default {
   name:"I'm So Cooked",
+  admin:{
+    tabs:[
+      {name:'Overview',url:'/'},
+      {name:'Users',url:'/users'},
+      {name:'Cuisines',url:'/cuisines'},
+      {name:'Regions',url:'/regions'},
+      {name:'Lessons',url:'/lessons'},
+      {name:'Problems',url:'/problems'},
+    ]
+  },
   roles:[
     "user",
     "admin"

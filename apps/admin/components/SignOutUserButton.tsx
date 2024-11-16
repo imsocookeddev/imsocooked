67c5f5612f303e6dc85ca4e030758a8fd963b73b
Christian Walker <christiandwalker2021@gmail.com>
2024-11-16 00:57:37 -0600

    updates signout button

--- apps/admin/components/SignOutUserButton.tsx
+++ apps/admin/components/SignOutUserButton.tsx
@@ -1,17 +1,20 @@
-'use client'
-import { DoorOpen } from "lucide-react";
+"use client";
+import { DoorOpen, LoaderCircle } from "lucide-react";
 import { DropdownMenuLabel } from "./ui/dropdown-menu";
 import { signUserOutAction } from "@/actions/clerk";
+import { useState } from "react";
 
 export default function SignOutUserButton() {
+  const [isClicked, setIsClicked] = useState(false);
   return (
     <DropdownMenuLabel
       className="flex flex-row w-full items-center justify-between cursor-pointer hover:bg-muted"
       onClick={async () => {
+        setIsClicked(true);
         await signUserOutAction();
       }}>
       <h3 className="text-red-600">Sign Out</h3>
-      <DoorOpen />
+      {isClicked ? <LoaderCircle className="animate-spin" /> : <DoorOpen />}
     </DropdownMenuLabel>
   );
 }

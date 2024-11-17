256979feb45d7d3cd3c7edf331e1fd59253a2724
Matthew <92887765+mjanderson1227@users.noreply.github.com>
2024-11-16 19:19:18 -0600

    Added create user function for the database

--- packages/config/cooked.config.ts
+++ packages/config/cooked.config.ts
@@ -1,24 +1,22 @@
 export default {
-  name:"I'm So Cooked",
-  admin:{
-    tabs:[
-      {name:'Overview',url:'/'},
-      {name:'Users',url:'/users'},
-      {name:'Cuisines',url:'/cuisines'},
-      {name:'Countries',url:'/countries'},
-      {name:'Lessons',url:'/lessons'},
-      {name:'Problems',url:'/problems'},
-      {name:"Problem Categories",url:'/categories'},
-    ]
+  name: "I'm So Cooked",
+  admin: {
+    tabs: [
+      { name: "Overview", url: "/" },
+      { name: "Users", url: "/users" },
+      { name: "Cuisines", url: "/cuisines" },
+      { name: "Countries", url: "/countries" },
+      { name: "Lessons", url: "/lessons" },
+      { name: "Problems", url: "/problems" },
+      { name: "Problem Categories", url: "/categories" },
+    ],
   },
-  roles:[
-    "user",
-    "admin"
-  ],
-  problem_types:[
+  roles: ["user", "admin"],
+  problem_types: [
     "mulitple_choice",
     "drag-n-drop",
     "matching",
     "single_word_response",
   ],
+  defaultHearts: 5,
 } as const;
--- packages/db/functions.ts
+++ packages/db/functions.ts
@@ -1,20 +1,53 @@
-import { db,eq,and } from ".";
+import { db, eq, and } from ".";
 import { user } from "./schema";
+import c from "@cooked/config";
 
-export async function getUser(id:string){
+export async function createUser({
+  id,
+  firstName,
+  lastName,
+  email,
+  username,
+}: {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
+}) {
+  let success = true;
+
+  try {
+    await db.insert(user).values({
+      userID: id,
+      role: "user",
+      hearts: c.defaultHearts,
+      email,
+      lastName,
+      firstName,
+      username,
+    });
+  } catch (e) {
+    console.error("Error occurred while inserting user data: " + e);
+    success = false;
+  }
+
+  return success;
+}
+
+export async function getUser(id: string) {
   return db.query.user.findFirst({
-    where:eq(user.userID,id)
-  })
+    where: eq(user.userID, id),
+  });
 }
 
-export async function getAdminUser(id:string){
+export async function getAdminUser(id: string) {
   return db.query.user.findFirst({
-    where:(
-      and(eq(user.userID,id), eq(user.role,'admin'))
-    )
-  })
+    where: and(eq(user.userID, id), eq(user.role, "admin")),
+  });
 }
 
-export async function getAllUsers(){
-  return db.query.user.findMany()
+export async function getAllUsers() {
+  return db.query.user.findMany();
 }
+

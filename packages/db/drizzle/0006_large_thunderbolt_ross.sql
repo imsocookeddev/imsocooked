6cc20b6376986d3dfbb5477412b99e7edf219dbb
Christian Walker <christiandwalker2021@gmail.com>
2024-11-24 19:31:56 -0600

    fix uuid migrations

--- packages/db/drizzle/0006_large_thunderbolt_ross.sql
+++ packages/db/drizzle/0006_large_thunderbolt_ross.sql
@@ -1,8 +1,8 @@
 ALTER TABLE "cuisine" ALTER COLUMN "imageUrl" SET DEFAULT 'https://cpsaxb6waydelzll.public.blob.vercel-storage.com/kitty-UVMd9CJBBFxJ1MUifj0lysaqPkUGUT.jpeg';
-ALTER TABLE "country_progress" ALTER COLUMN "countryID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "cuisine_progress" ALTER COLUMN "cuisineID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "cuisines_to_countries" ALTER COLUMN "cuisineID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "cuisines_to_countries" ALTER COLUMN "countryID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "lesson" ALTER COLUMN "countryID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "lesson" ALTER COLUMN "cuisineID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "country_progress" DROP COLUMN IF EXISTS "cuisineID";
+-- ALTER TABLE "country_progress" ALTER COLUMN "countryID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
+-- ALTER TABLE "cuisine_progress" ALTER COLUMN "cuisineID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
+-- ALTER TABLE "cuisines_to_countries" ALTER COLUMN "cuisineID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
+-- ALTER TABLE "cuisines_to_countries" ALTER COLUMN "countryID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
+-- ALTER TABLE "lesson" ALTER COLUMN "countryID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
+-- ALTER TABLE "lesson" ALTER COLUMN "cuisineID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
+-- ALTER TABLE "country_progress" DROP COLUMN IF EXISTS "cuisineID";
---
+++ packages/db/drizzle/0010_secret_darwin.sql
@@ -0,0 +1,9 @@
+ALTER TABLE "problem_completion" ALTER COLUMN "problemID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
+ALTER TABLE "problems_to_categories" ALTER COLUMN "problemID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
+ALTER TABLE "country_progress" ALTER COLUMN "countryID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
+ALTER TABLE "cuisine_progress" ALTER COLUMN "cuisineID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
+ALTER TABLE "cuisines_to_countries" ALTER COLUMN "cuisineID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
+ALTER TABLE "cuisines_to_countries" ALTER COLUMN "countryID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
+ALTER TABLE "lesson" ALTER COLUMN "countryID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
+ALTER TABLE "lesson" ALTER COLUMN "cuisineID" SET DATA TYPE uuid USING gen_random_uuid();--> statement-breakpoint
+ALTER TABLE "country_progress" DROP COLUMN IF EXISTS "cuisineID";
